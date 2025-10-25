57cbe9d607118a638f6b2dcddac5ef0abf4325ae
Andrés-ALSE <elwitchcastlero@gmail.com>
2025-10-25 00:49:53 -0500

    Update Biblioteca.cpp

--- src/Biblioteca.cpp
+++ src/Biblioteca.cpp
@@ -2,15 +2,17 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-void Biblioteca::agregar(const libro& Libro) {
+//la clase solo fue definida en Biblioteca.h para facilidad de compilación al no ser tan extenso al darme problemas
+//la unica variable a manejar es (vector<libro> libros) al este ser la biblioteca donde se guardan los libros, tambien fue definida solo en Biblioteca.h
+//por facilidad de uso
+void Biblioteca::agregar(const libro& Libro) { //pide por su propia cuenta el libro y se encarga de añadirlo a la biblioteca
     libros.push_back(Libro);
 }
 
-void Biblioteca::quitar(const libro& Libro) {
-    bool encontrado = false;
-    for (auto it = libros.begin(); it != libros.end(); ++it) {
-        if (it->getisbn_sn() == Libro.getisbn_sn()) {
+void Biblioteca::quitar(const libro& Libro) {      //metodo para quitar libros de la biblioteca
+    bool encontrado = false;                       //definimos una variable para determinar si es encontrado o no
+    for (auto it = libros.begin(); it != libros.end(); ++it) {  //usamos un puntero para tener acceso a la memoria del vector y poderlo cambiar de manera facil
+        if (it->getisbn_sn() == Libro.getisbn_sn()) {   //para realizar la 
             libros.erase(it);
             encontrado = true;
             cout << "Libro eliminado.\n";
